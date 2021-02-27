25ac9a7989ebab26400ca8581de7776fe986c868
jskim91 <jskim91@starlabs.co.kr>
2021-02-27 19:32:43 +0900

    pre-redering

--- components/layout.js
+++ components/layout.js
@@ -29,12 +29,12 @@ export default function Layout({ children, home }) {
                 {home ? (
                 <>
                     <Image
-                    priority
-                    src="/images/profile.jpg"
-                    className={utilStyles.borderCircle}
-                    height={144}
-                    width={144}
-                    alt={name}
+                        priority
+                        src="/images/profile.jpg"
+                        className={utilStyles.borderCircle}
+                        height={144}
+                        width={144}
+                        alt={name}
                     />
                     <h1 className={utilStyles.heading2Xl}>{name}</h1>
                 </>
@@ -43,12 +43,12 @@ export default function Layout({ children, home }) {
                     <Link href="/">
                     <a>
                         <Image
-                        priority
-                        src="/images/profile.jpg"
-                        className={utilStyles.borderCircle}
-                        height={108}
-                        width={108}
-                        alt={name}
+                            priority
+                            src="/images/profile.jpg"
+                            className={utilStyles.borderCircle}
+                            height={108}
+                            width={108}
+                            alt={name}
                         />
                     </a>
                     </Link>
